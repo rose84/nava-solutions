66e284f562f75b5ee65e5ab1f6b7d1c2ae8af5f8
Juan <juan@villela.co>
2019-02-11 12:38:39 -0500

    Added embedded Wufoo Quote form.

---
+++ assets/js/wufoo.js
@@ -0,0 +1,16 @@
+var z1kl2dfg0nf9vav;(function(d, t) {
+  var s = d.createElement(t), options = {
+  'userName':'navasolutions',
+  'formHash':'z1kl2dfg0nf9vav',
+  'autoResize':true,
+  'height':'921',
+  'async':true,
+  'host':'wufoo.com',
+  'header':'show',
+  'ssl':true};
+  s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'secure.wufoo.com/scripts/embed/form.js';
+  s.onload = s.onreadystatechange = function() {
+  var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+  try { z1kl2dfg0nf9vav = new WufooForm();z1kl2dfg0nf9vav.initialize(options);z1kl2dfg0nf9vav.display(); } catch (e) {}};
+  var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+})(document, 'script');
